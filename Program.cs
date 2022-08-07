6e92c6f39255b4498b03a7d01fbafb9924b97387
AnatoliySokolov-code <109688326+AnatoliySokolov-code@users.noreply.github.com>
2022-08-07 14:01:37 +0300

    ервая задача математически

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿/* Напишите программу,
+которая на вход принимает число
+и выдаёт его квадрат
+*/
+
+Console.Write("Введите число");
+int number = int.Parse( Console.ReadLine());
+
+// Математически 
+int sqr = number * number;
+Console.WriteLine("Вывод математическим методом: ");
+Console.WriteLine($"Квадрат числа {number} равен = {sqr}");
