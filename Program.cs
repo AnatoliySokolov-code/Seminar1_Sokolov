a1599ba3bee3c58dfe298dac38da3e9391fd4c7b
AnatoliySokolov-code <109688326+AnatoliySokolov-code@users.noreply.github.com>
2022-08-07 14:22:32 +0300

    ервая задача с помощью библиотеки

--- Program.cs
+++ Program.cs
@@ -10,3 +10,8 @@ int number = int.Parse( Console.ReadLine());
 int sqr = number * number;
 Console.WriteLine("Вывод математическим методом: ");
 Console.WriteLine($"Квадрат числа {number} равен = {sqr}");
+
+// С помощью библиотеки
+int sqr1 = Convert.ToInt32(Math.Pow(number,2));
+Console.WriteLine("Вывод с помощью библиотеки: ");
+Console.WriteLine($"Квадрат числа {number}  равен = {sqr1}");
