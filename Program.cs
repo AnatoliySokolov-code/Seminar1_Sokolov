d1df1c81e7d754ca953a7c12529429836dd397bb
AnatoliySokolov-code <109688326+AnatoliySokolov-code@users.noreply.github.com>
2022-08-07 15:02:08 +0300

    Задачу 1 решаю

--- Program.cs
+++ Program.cs
@@ -1,17 +1,1 @@
-﻿/* Напишите программу,
-которая на вход принимает число
-и выдаёт его квадрат
-*/
-
-Console.Write("Введите число");
-int number = int.Parse( Console.ReadLine());
-
-// Математически 
-int sqr = number * number;
-Console.WriteLine("Вывод математическим методом: ");
-Console.WriteLine($"Квадрат числа {number} равен = {sqr}");
-
-// С помощью библиотеки
-int sqr1 = Convert.ToInt32(Math.Pow(number,2));
-Console.WriteLine("Вывод с помощью библиотеки: ");
-Console.WriteLine($"Квадрат числа {number}  равен = {sqr1}");
+﻿
